b322685b8dc718555991c017dfbbcbcd61a962c4
Gun-93 <gunpcity@gmail.com>
2026-04-07 21:31:31 +0530

    fixed cors

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000","https://snnapeatss.netlify.app/"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
